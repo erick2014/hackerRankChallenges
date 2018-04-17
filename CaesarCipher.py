2fb5023822a7887ca17a0156203a980417249626
erick <erickarmandogarcia@hotmail.com>
2018-04-17 10:04:16 -0500

    Finish CaesarCipher challenge

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,17 +1,38 @@
+'''
+	test case 1 input:
+	10
+	159357lcfd
+	98
+
+	test case 1 output:
+	159357fwzx
+
+	test case 2 input:
+	11
+	middle-Outz
+	2
+
+	test case 2 output
+	okffng-Qwvb
+'''
+
 def caesarCipher(s, k):
 	# concatenate encrypted characteres
 	phraseEncrypted = ''
 	alphabet = 	 'abcdefghijklmnopqrstuvwxyz'
-	# get the alphabet size
-	alphabetSize = len(alphabet)
+	sizeToRotate = 0 
+
+	if k >= len(alphabet) : sizeToRotate = k % len(alphabet)
+	else: sizeToRotate = k
+	
 	# join the alphabeth rotating the positions we received as param
-	alphabetRotated = alphabet[k::]+alphabet[:k]
+	alphabetRotated = alphabet[sizeToRotate::]+alphabet[:sizeToRotate]
 	
 	# loop each letter from the phrase
 	for letter in s:
-		# look for the letter in the normal alphabet
+			# look for the letter in the normal alphabet
 		letterFound = alphabet.find(letter.lower())
-		
+			
 		if(letterFound!=-1) :	
 			# get the encrypted letter
 			letterEncrypted= alphabetRotated[letterFound]
