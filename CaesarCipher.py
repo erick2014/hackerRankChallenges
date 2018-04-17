908c7bd722d3d69a18764d7343b7babf7582f25a
erick <erickarmandogarcia@hotmail.com>
2018-04-17 08:27:13 -0500

    Refactor CaesarCipher implementation

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,37 +1,30 @@
 def caesarCipher(s, k):
-    # concatenate phrase encrypted
-    phraseEncrypted = ''
-    alphabet = 	 ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    # get the alphabet size
-    alphabetSize = len(alphabet)
-
-    # split the list from the rotation num to the end of the list
-    alphabetWithRotation = alphabet[k::]
-    # split the list from the rotation number to the beginning of the list
-    alphabetRotatedStart = alphabet[:k]
-    # join the to lists
-    alphabhetRotated = alphabetWithRotation+alphabetRotatedStart
-    
-    # loop each letter from the phrase
-    for letter in s:
-        currentLetter = letter
-        # catch any exception if the item was not found
-        try:
-            # look for the letter in the normal alphabet
-            letterFound = alphabet.index(currentLetter.lower(),0,alphabetSize)
-            # get the encrypted letter
-            letterEncrypted= alphabhetRotated[letterFound]
-            # current letter is upper?
-            if currentLetter.isupper() :
-                # if is upper concatenate the latter as it is
-                phraseEncrypted+=letterEncrypted.upper()
-            else:
-                phraseEncrypted+=letterEncrypted
-
-        except ValueError:
-            phraseEncrypted+=currentLetter
-    
-    return phraseEncrypted
+	# concatenate encrypted characteres
+	phraseEncrypted = ''
+	alphabet = 	 'abcdefghijklmnopqrstuvwxyz'
+	# get the alphabet size
+	alphabetSize = len(alphabet)
+	# join the alphabeth rotating the positions we received as param
+	alphabetRotated = alphabet[k::]+alphabet[:k]
+	
+	# loop each letter from the phrase
+	for letter in s:
+		# look for the letter in the normal alphabet
+		letterFound = alphabet.find(letter.lower())
+		
+		if(letterFound!=-1) :	
+			# get the encrypted letter
+			letterEncrypted= alphabetRotated[letterFound]
+			# current letter is upper?
+			if letter.isupper() :
+				# if is upper concatenate the latter as it is
+				phraseEncrypted+=letterEncrypted.upper()
+			else:
+				phraseEncrypted+=letterEncrypted
+		else:
+			phraseEncrypted+=letter
+	
+	return phraseEncrypted
 		
 
 if __name__ == "__main__":
