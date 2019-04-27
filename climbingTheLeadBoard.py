c309c7a26a4948fdf2a5b5bd749b3d3d35570da2
erick2014 <erickarmandogarcia@hotmail.com>
2019-04-27 16:05:39 -0500

    find rankings for alice without creating a copy of the scores

--- climbingTheLeadBoard.py
+++ climbingTheLeadBoard.py
@@ -1,72 +1,54 @@
 #!/usr/local/bin/python3
-from math import floor
 
-def binarySearch (itemList, valueToFind) :
-    start = 0
-    stop = len(itemList) -1 
-    middle = floor((start + stop) / 2)
-
-    while (itemList[middle] != valueToFind and start < stop) :
-         
-        if valueToFind < itemList[middle] :
-            start = middle + 1
-        else:
-            stop = middle - 1
-    
-        middle = floor((start + stop) / 2)
-
-    #if the current middle item is what we're looking for return it's index, else return -1
-    if itemList[middle] != valueToFind:
-        return -1
-    else:
-        return middle
-
-def getRankingsFromScores(scores):
+def getRankingsFromScores(scores,aliceScore):
+    aliceRankingPosition = 0
     currentRanks = [1]
-    
+    greaterScore = 0
+    greaterRankingPos = 0
+
     for i in range(1,len(scores)):
         currentScore = i
         previousScore = currentScore -1
+
+        currentScoreValue = scores[currentScore]
+        previousScoreValue = scores[previousScore]
         
         if(scores[currentScore] == scores[previousScore]):
             newScore= currentRanks[previousScore]
+
+            if currentScoreValue > aliceScore:
+                greaterScore = currentScoreValue
+                greaterRankingPos = currentRanks[previousScore]
+
         else:
             newScore = (currentRanks[previousScore])+1
+            
+            if previousScoreValue > aliceScore:
+                greaterScore = previousScoreValue
+                greaterRankingPos = currentRanks[previousScore]
+
+            if currentScoreValue > aliceScore:
+                greaterScore = currentScoreValue
+                greaterRankingPos = currentRanks[previousScore]+1
 
         currentRanks.append(newScore)
+
+    if greaterScore == 0:
+        aliceRankingPosition = 1
     
-    return currentRanks
+    if aliceScore < greaterScore:
+        aliceRankingPosition = greaterRankingPos+1
+
+    return aliceRankingPosition
+
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, aliceScores):
-    currentRankings = getRankingsFromScores(scores)
     aliceRankings = []
-
+    
     for aliceScore in aliceScores:
-        newScores = scores.copy()
-        # append the alice's score to the current scores
-        newScores.append(aliceScore)
-        # sort the scores in descending order
-        newScores.sort()
-        newScores = newScores[::-1]
-
-        positionForAddedScore = binarySearch(newScores,aliceScore)
-        positionBeforeForNewScore = positionForAddedScore-1
-        newRanking = 0
-        print('newScores ',newScores)
-        print('aliceScore ',aliceScore)
-        print('positionForAddedScore ',positionForAddedScore)
-
-        if positionBeforeForNewScore <= 0 :
-            aliceRankings.append(1)
-
-        elif newScores[positionBeforeForNewScore] == newScores[positionForAddedScore] :
-            newRanking = currentRankings[positionBeforeForNewScore]
-            aliceRankings.append(newRanking) 
-
-        elif newScores[positionBeforeForNewScore] != newScores[positionForAddedScore]:
-            newRanking = currentRankings[positionBeforeForNewScore]+1
-            aliceRankings.append(newRanking)
+        aliceRankingPosition = getRankingsFromScores(scores,aliceScore)
+        aliceRankings.append(aliceRankingPosition)
 
     return aliceRankings
 
@@ -77,35 +59,35 @@ aliceGames = 0
 aliceScores = []
 
 # read line by line the config needed
-# scoresInFile = open("climbingTheBoard200000Input.txt","r")
+scoresInFile = open("climbingTheBoard200000Input.txt","r")
 
-# with scoresInFile as sB:
-#     lineCounter = 1
+with scoresInFile as sB:
+    lineCounter = 1
     
-#     for line in sB:
-#         scoreLine=[]
+    for line in sB:
+        scoreLine=[]
 
-#         if(lineCounter==1):
-#             scoresCount = int(line)
+        if(lineCounter==1):
+            scoresCount = int(line)
 
-#         if(lineCounter==2):
-#             scoreLine =line.rstrip().split()
-#             scores = list(map(int,scoreLine))
+        if(lineCounter==2):
+            scoreLine =line.rstrip().split()
+            scores = list(map(int,scoreLine))
 
-#         if(lineCounter==3):
-#             aliceGames = line.rstrip().split()
-#             aliceGames = int(aliceGames[0])
+        if(lineCounter==3):
+            aliceGames = line.rstrip().split()
+            aliceGames = int(aliceGames[0])
 
-#         if(lineCounter==4):
-#             scoreLine =line.rstrip().split()
-#             aliceScores = list(map(int,scoreLine))
+        if(lineCounter==4):
+            scoreLine =line.rstrip().split()
+            aliceScores = list(map(int,scoreLine))
 
-#         lineCounter+=1
+        lineCounter+=1
 
-# scoresInFile.close()
+scoresInFile.close()
 
-scores = [100,90,90,80,75,60]
-aliceScores = [50,65,77,90,102]
+# scores = [100,90,90,80,75,60]
+# aliceScores = [50,65,77,90,102]
 
 result = climbingLeaderboard(scores, aliceScores)
 print(result)
